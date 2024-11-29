5a6dfc64fe44c245c9a946460d40a6ef4d83642f
suraj-05042005-maurya <surajara96@gmail.com>
2024-11-29 13:16:44 +0530

    Add files via upload

---
+++ 00000.py
@@ -0,0 +1,12 @@
+                 #    Build Your Logic In COding
+
+# write a program to print multiplication table of user choice.
+
+# Make a variable to take input from user.
+num = int(input("Please Enter a number to print table of that number: "))
+
+# write your logic to print table.
+for i in range(1, 11):
+    print(f"{num} * {i} = {num * i}")
+
+                 # Try this code in your code editor.
